796e5baf4d60ccbffe78f5112c29cad1aeb01dd5
Danilo Morales <gadaproductions@Danilos-MacBook-Pro.local>
2017-02-08 14:51:24 -0500

    worked on the photos pages

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,3 @@
 export const FireBaseConfig = {
 
-    apiKey: "AIzaSyCkZFD9FKt5MC9CZPqxhX2uw4eCdbUj9Yg",
-    authDomain: "gadaphotos-86474.firebaseapp.com",
-    databaseURL: "https://gadaphotos-86474.firebaseio.com",
-    storageBucket: "gadaphotos-86474.appspot.com",
-    messagingSenderId: "643804618298"
 }
